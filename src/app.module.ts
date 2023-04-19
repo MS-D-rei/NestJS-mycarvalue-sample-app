e11f8e584ad397ca556f7b918d232b1e376d7491
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-04-19 17:29:26 +0900

    add config module and typeorm module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,38 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
+import { UsersModule } from '@/users/users.module';
+import { User } from '@/users/users.entity';
+import { ReportsModule } from '@/reports/reports.module';
+import mysqlConfig from 'mysql-configuration';
+
+console.log(process.env.DB_PORT);
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [mysqlConfig],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: configService.get('database.database'),
+        entities: [User],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
