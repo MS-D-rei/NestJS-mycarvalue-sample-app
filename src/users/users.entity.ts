c52281bdc92cfd9e60e918ab1bc8f11eb737d2c1
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-04-19 20:52:45 +0900

    modify CreateDateColumn and UpdateDateColumn

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,17 +1,28 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
+
   @Column()
   username: string;
+
   @Column()
   email: string;
+
   @Column()
   password: string;
-  @Column()
+
+  @CreateDateColumn()
   createdAt: Date;
-  @Column()
+
+  @UpdateDateColumn()
   updatedAt: Date;
 }
