aae53a76904b56135edce4332045ca27e690324d
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-04-19 17:28:48 +0900

    add TypeOrmModule.forFeature user to users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersService } from '@/users/users.service';
+import { User } from '@/users/users.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [UsersService],
 })
 export class UsersModule {}
