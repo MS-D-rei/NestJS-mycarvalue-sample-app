240cac94ba035b5a341347f848c7bf4bd6fe1585
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-04-19 21:06:02 +0900

    create users and reports controller

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Report } from './reports.entity';
+import { ReportsController } from './reports.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Report])],
   providers: [ReportsService],
+  controllers: [ReportsController],
 })
 export class ReportsModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from '@/users/users.service';
 import { User } from '@/users/users.entity';
+import { UsersController } from './users.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   providers: [UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
