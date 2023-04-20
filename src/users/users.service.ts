314997aaa805cfbf5f34410ea4695bee190f2630
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-04-20 15:17:06 +0900

    add create and findOne func to user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '@/users/users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async create(username: string, email: string, password: string) {
+    const user = this.usersRepository.create({ username, email, password });
+    return this.usersRepository.save(user);
+  }
+
+  async findOne(id: number) {
+    return this.usersRepository.findOneBy({ id });
+  }
+}
