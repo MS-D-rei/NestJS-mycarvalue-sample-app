cef5d3272f62c8a701557a131dd516e05b031f8d
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-04-23 00:54:09 +0900

    remove @Exclude password from user entity

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,4 +1,3 @@
-import { Exclude } from 'class-transformer';
 import {
   AfterInsert,
   AfterRemove,
@@ -22,7 +21,6 @@ export class User {
   email: string;
 
   @Column()
-  @Exclude()
   password: string;
 
   @CreateDateColumn()
