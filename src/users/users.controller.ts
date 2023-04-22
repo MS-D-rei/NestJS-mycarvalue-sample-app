0f9ced5172e1631a7fc41975b46a877745851c76
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-04-23 00:56:24 +0900

    apply serialize interceptor to findOne in user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,17 @@
 import {
   Body,
-  ClassSerializerInterceptor,
   Controller,
   Delete,
   Get,
   NotFoundException,
   Param,
+  ParseIntPipe,
   Patch,
   UseInterceptors,
 } from '@nestjs/common';
 import { UsersService } from '@/users/users.service';
 import { UpdateUserDto } from '@/users/dto/update-user.dto';
+import { SerializeInterceptor } from '@/interceptor/serialize.interceptor';
 
 @Controller('users')
 export class UsersController {
@@ -21,9 +22,10 @@ export class UsersController {
     return this.usersService.findAll();
   }
 
-  @UseInterceptors(ClassSerializerInterceptor)
+  @UseInterceptors(SerializeInterceptor)
   @Get(':id')
-  async findUser(@Param('id') id: string) {
+  async findUser(@Param('id', ParseIntPipe) id: string) {
+    console.log('handler is running');
     const user = await this.usersService.findOne(parseInt(id));
     if (!user) {
       throw new NotFoundException('User not found');
