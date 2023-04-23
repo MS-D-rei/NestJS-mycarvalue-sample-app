2eeae34672423e6747dfebf8c3bfa562b5ed11e6
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-04-23 19:01:30 +0900

    apply Serialize decorator to findUser in users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,11 +7,11 @@ import {
   Param,
   ParseIntPipe,
   Patch,
-  UseInterceptors,
 } from '@nestjs/common';
 import { UsersService } from '@/users/users.service';
 import { UpdateUserDto } from '@/users/dto/update-user.dto';
-import { SerializeInterceptor } from '@/interceptor/serialize.interceptor';
+import { Serialize } from '@/interceptor/serialize.interceptor';
+import { UserDto } from './dto/user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -22,10 +22,9 @@ export class UsersController {
     return this.usersService.findAll();
   }
 
-  @UseInterceptors(SerializeInterceptor)
+  @Serialize(UserDto)
   @Get(':id')
   async findUser(@Param('id', ParseIntPipe) id: string) {
-    console.log('handler is running');
     const user = await this.usersService.findOne(parseInt(id));
     if (!user) {
       throw new NotFoundException('User not found');
