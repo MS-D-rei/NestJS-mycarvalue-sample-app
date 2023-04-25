3ef1d1543b3c108921a6283a5b6723d270e4e869
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-04-25 19:31:02 +0900

    modify jest config

--- jest.config.ts
+++ jest.config.ts
@@ -2,11 +2,17 @@ import { Config } from 'jest';
 
 const config: Config = {
   testEnvironment: 'node',
+  roots: ['<rootDir>/src/', '<rootDir>/test/'],
   testMatch: ['**/*.spec.ts'],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
+    '<rootDir>/db/',
+    '<rootDir>/env/',
+  ],
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
-  roots: ['<rootDir>/src/', '<rootDir>/test/'],
   rootDir: '.',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
