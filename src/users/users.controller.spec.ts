4c2fc15419572bcec84accada036309ba37cd667
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-05-01 00:03:01 +0900

    add test for each handler in users controller spec

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,96 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UsersController } from './users.controller';
+import { UsersController } from '@/users/users.controller';
+import { UsersService } from '@/users/users.service';
+import { User } from './users.entity';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+const registeredUsers: Partial<User>[] = [
+  {
+    id: 1,
+    username: 'testUser1',
+    email: 'user1@email.com',
+    password: 'testPassword1',
+  },
+  {
+    id: 2,
+    username: 'testUser2',
+    email: 'user2@email.com',
+    password: 'testPassword2',
+  },
+];
 
 describe('UsersController', () => {
-  let controller: UsersController;
+  let usersController: UsersController;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: {
+            findAll: jest.fn().mockResolvedValue(registeredUsers),
+            findOne: jest.fn(),
+            findByEmail: jest.fn(),
+            create: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<UsersController>(UsersController);
+    usersController = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(usersController).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of users', async () => {
+      expect(await usersController.findAll()).toEqual(registeredUsers);
+      expect(usersService.findAll).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a user by id', async () => {
+      const user = registeredUsers[0];
+      jest.spyOn(usersService, 'findOne').mockResolvedValue(user as User);
+      const result = await usersController.findOne(user.id.toString());
+      expect(result).toEqual(user);
+      expect(usersService.findOne).toHaveBeenCalledWith(user.id);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a user by id with data', async () => {
+      const user = registeredUsers[0];
+      jest.spyOn(usersService, 'update').mockResolvedValue(user as User);
+      const result = await usersController.update(user.id.toString(), {
+        username: 'testUser1-updated',
+      } as UpdateUserDto);
+      expect(result).toEqual(user);
+      expect(usersService.update).toHaveBeenCalledWith(user.id, {
+        username: 'testUser1-updated',
+      });
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove a user by id', async () => {
+      const user = registeredUsers[0];
+      jest.spyOn(usersService, 'remove').mockResolvedValue(user as User);
+      const result = await usersController.remove(user.id.toString());
+      expect(result).toEqual(user);
+      expect(usersService.remove).toHaveBeenCalledWith(user.id);
+    });
   });
 });
