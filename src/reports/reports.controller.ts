3cd3c771f02ac4a5c7dfad914c8f6d64653329be
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-05-05 18:43:35 +0900

    add frame of handlers in reports controller

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  @Post()
+  create() {
+    return 'This action creates a new report';
+  }
+
+  @Get()
+  findAll() {
+    return 'This action returns all reports';
+  }
+
+  @Get(':id')
+  findOne() {
+    return 'This action returns a #${id} report';
+  }
+
+  @Patch(':id')
+  update() {
+    return 'This action updates a #${id} report';
+  }
+
+  @Delete(':id')
+  remove() {
+    return 'This action removes a #${id} report';
+  }
+}
