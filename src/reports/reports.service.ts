680ba64d51d5d4e935b151504d4602ea4f41e251
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-05-05 18:46:13 +0900

    add frame of functions in reports service

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Report } from '@/reports/reports.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    @InjectRepository(Report)
+    private reportsRepository: Repository<Report>,
+  ) {}
+
+  create() {
+    return 'This action creates a new report';
+  }
+
+  findAll() {
+    return 'This action returns all reports';
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} report`;
+  }
+
+  update(id: number) {
+    return `This action updates a #${id} report`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} report`;
+  }
+}
