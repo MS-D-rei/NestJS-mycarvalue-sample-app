e8febde7417e68cc25d675a4ec557906ba4e7721
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-05-10 01:17:20 +0900

    add UsersModule to imports in reports module

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Report } from '@/entities';
 import { ReportsController } from './reports.controller';
 import { CurrentUserMiddleware } from '@/users/middleware/current-user.middleware';
+import { UsersModule } from '@/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Report])],
+  imports: [TypeOrmModule.forFeature([Report]), UsersModule],
   providers: [ReportsService],
   controllers: [ReportsController],
 })
