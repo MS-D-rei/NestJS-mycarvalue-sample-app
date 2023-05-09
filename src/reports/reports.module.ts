8f2ec44d68e629136e82206baf2c6fc510f93e59
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-05-09 19:16:33 +0900

    apply current user middleware to reports module

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Report } from '@/entities';
 import { ReportsController } from './reports.controller';
+import { CurrentUserMiddleware } from '@/users/middleware/current-user.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Report])],
   providers: [ReportsService],
   controllers: [ReportsController],
 })
-export class ReportsModule {}
+export class ReportsModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CurrentUserMiddleware).forRoutes('*');
+  }
+}
